11f835f85e0c547cc5bdbc75d260a9ce576c1e8b
dangquangdon <dangquangdon@gmail.com>
2024-02-04 01:19:21 +0200

    re-organize

--- pg/pg.py
+++ pg/pg.py
@@ -1,13 +1,14 @@
 import psycopg2
 import os
 import valohai
+import json
 
 
 def connect():
     conn_str = os.environ.get("PG_CONN_STR")
     conn = psycopg2.connect(conn_str)
 
-    query_sql = 'SELECT VERSION()'
+    query_sql = valohai.parameters("sql_query").value
 
     cur = conn.cursor()
     cur.execute(query_sql)
@@ -17,6 +18,13 @@ def connect():
     output = valohai.outputs().path("output.txt")
     with open(output, "w") as file:
         file.write(version)
+    
+    metadata = {
+        "valohai.alias": valohai.parameters("datum_alias").value
+    }
+    metadata_path = valohai.outputs().path(f"{output}.metadata.json")
+    with open(metadata_path, "w") as metadata_out:
+        json.dump(metadata, metadata_out)
 
 if __name__ == '__main__':
     connect()
