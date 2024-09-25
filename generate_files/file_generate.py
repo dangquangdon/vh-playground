2a723a4e784c3e6033edc77d39c8d2360cbdcdb1
dangquangdon <dangquangdon@gmail.com>
2024-09-25 22:52:30 +0300

    fix dataset and version name

--- generate_files/file_generate.py
+++ generate_files/file_generate.py
@@ -14,8 +14,13 @@ def generate_random_file_content(size_in_bytes):
 
 
 def generate_random_files(num_files, min_size, max_size):
-    dataset_name = valohai.parameters("dataset-name").value
-    dataset_version = valohai.parameters("dataset-version").value
+    dataset_name = str(valohai.parameters("dataset_name").value).strip()
+    dataset_version = str(valohai.parameters("dataset_version").value).strip()
+    metadata = {
+        "valohai.dataset-versions": [
+            f"dataset://{dataset_name}/{dataset_version}",
+        ]
+    }
     for i in range(1, num_files + 1):
         # Generate a random file size within the specified range
         file_size = random.randint(min_size, max_size)
@@ -27,18 +32,15 @@ def generate_random_files(num_files, min_size, max_size):
         output = valohai.outputs().path(f"random_file_{i}.txt")
         with open(output, "wb") as f:
             f.write(content)
-        metadata = {
-            "valohai.dataset-versions": [f"dataset://{dataset_name}/{dataset_version}"]
-        }
-        metadata_path = f'/valohai/outputs/random_file_{i}.metadata.json'
-        with open(metadata_path, 'w') as outfile:
+
+        metadata_path = f"/valohai/outputs/random_file_{i}.metadata.json"
+        with open(metadata_path, "w") as outfile:
             json.dump(metadata, outfile)
 
         print(f"Created {output} with size {file_size} bytes.")
 
-
-if __name__ == "__main__":
     
+if __name__ == "__main__":
     num_files = int(valohai.parameters("number_of_files").value)
     min_file_size = int(valohai.parameters("min_file_size").value)
     max_file_size = int(valohai.parameters("max_file_size").value)
