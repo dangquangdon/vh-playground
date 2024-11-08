88a1f8d5f13c26781a2852415b355731ea1a21d6
dangquangdon <dangquangdon@gmail.com>
2024-11-08 10:14:29 +0200

    add iteration for confusion

--- confusion/confusion.py
+++ confusion/confusion.py
@@ -1,10 +1,17 @@
 from sklearn.metrics import confusion_matrix
 import numpy as np
 import json
+import valohai
 
-    
-actual = np.random.binomial(1,.9,size = 1000)
-predicted = np.random.binomial(1,.9,size = 1000)
-matrix = confusion_matrix(actual, predicted)
 
-print(json.dumps({"data": matrix.tolist()}))
+def generate_random_confusion():   
+    actual = np.random.binomial(1,.9,size = 1000)
+    predicted = np.random.binomial(1,.9,size = 1000)
+    matrix = confusion_matrix(actual, predicted)
+    return matrix.tolist()
+
+
+if __name__ == "__main__":
+    iterations = int(valohai.parameters("iterations").value)
+    for i in range(iterations):
+        print(json.dumps({f"data{i+1}": generate_random_confusion()}))
